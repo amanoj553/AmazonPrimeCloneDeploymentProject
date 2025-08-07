13af553518922ba4d1e88e667ad3e94c09f5c41c
amanoj553 <33591780+amanoj553@users.noreply.github.com>
2025-08-08 02:37:53 +0530

    Update eks.tf

--- terraform_code/eks_code/eks.tf
+++ terraform_code/eks_code/eks.tf
@@ -1,8 +1,24 @@
+locals {
+  region          = "us-east-1"
+  name            = "amazon-prime-cluster"
+  vpc_cidr        = "10.0.0.0/16"
+  azs             = ["us-east-1a", "us-east-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+  intra_subnets   = ["10.0.5.0/24", "10.0.6.0/24"]
+
+  tags = {
+    Environment = "dev"
+    Example     = local.name
+  }
+}
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.15.1"
 
   cluster_name                   = local.name
+  cluster_version                = "1.29"
   cluster_endpoint_public_access = true
 
   cluster_addons = {
@@ -17,8 +33,8 @@ module "eks" {
     }
   }
 
-  vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_groups = {
     panda-node = {
@@ -30,7 +46,8 @@ module "eks" {
       capacity_type  = "SPOT"
 
       tags = {
-        ExtraTag = "Panda_Node"
+        Name      = "panda-node"
+        ExtraTag  = "Panda_Node"
       }
     }
   }
